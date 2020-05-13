e72b1e738c3841dff4b166ea4604be0cae253502
mattted <medward4@gmail.com>
2020-05-13 09:10:51 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-animal = ""
+let animal = "cat"
 
 function myAnimal() {
+  let animal = "dog"
   return animal
 }
 
@@ -11,9 +12,9 @@ function yourAnimal() {
   return animal
 }
 
+let two = 2
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  two = 2
 }
